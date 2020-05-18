6684edcf79c97220b499b33d3d434050d7a6b1b7
CodeyPacker <codeypacker@gmail.com>
2020-05-17 23:37:35 -0600

    Set up videoList component and looped over video titles

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,13 +1,22 @@
 <template>
   <ul>
-    Video list!
+    <VideoListItem
+      v-for="video in videos"
+      :video="video"
+      :key="video.etag"
+    >
+    </VideoListItem>
   </ul>
 </template>
 
 <script>
+  import VideoListItem from './VideoListItem'
   export default {
     name: 'VideoList',
-    props: ['videos']
+    components: {
+      VideoListItem
+    },
+    props: ['videos'],
   }
 </script>
 
